1aa0ff4522fc0294b8e3a2f063a2ce203a7ea48e
Tamim-369 <ashiqurrahmantamim369@gmail.com>
2025-03-11 01:58:55 -0400

    fixing some issues

--- main.py
+++ main.py
@@ -6,12 +6,10 @@ class App(customtkinter.CTk):
         self.geometry("400x200")
         self.title("CustomTkinter Example")
         self.label  = customtkinter.CTkLabel(self, text="Enter details about the person you want to become.", text_color="gray").pack()
-        # Creating a Textbox (Multiline Input)
         self.input = customtkinter.CTkTextbox(self, height=100, width=300)
         self.input.pack(padx=20, pady=10)
 
 
-        # Button
         self.button = customtkinter.CTkButton(self, text="Start", command=self.button_callback)
         self.button.pack(padx=20, pady=10)
 
